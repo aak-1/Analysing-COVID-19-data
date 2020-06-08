07024b586e949f4e82ba3dae3d07c3b22ee84e6c
Aakriti Aggarwal <aggarwal.aa.01@gmail.com>
2020-06-08 16:26:03 +0530

    Delete unnecessary files

--- Automate commits.py
+++
@@ -1,6 +0,0 @@
-import subprocess as cmd
-
-cp = cmd.run("git add .", check=True, shell=True)
-cp = cmd.run(f"git commit -m \"update repo\"", check=True, shell=True)
-cp = cmd.run("git push", check=True, shell=True)
-pawse = input("Done!")
